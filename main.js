0865afc396dda9490ab499b2f7eef81006edc6d9
Queued Pixel <git@queuedpixel.com>
2018-01-01 18:46:08 -0800

    Added initial version of Electron application.

---
+++ main.js
@@ -0,0 +1,44 @@
+/*
+
+stream-countdown-overlay : Countdown Overlay for Twitch Streams
+
+Copyright (c) 2018 Queued Pixel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+var electron = require( "electron" );
+var path     = require( "path"     );
+var url      = require( "url"      );
+
+function start()
+{
+    var window = new electron.BrowserWindow( { autoHideMenuBar : true } );
+    window.loadURL(
+            url.format( { pathname: path.join( __dirname, "overlay.html" ), protocol: 'file:', slashes: true } ));
+};
+
+function stop()
+{
+    electron.app.quit();
+};
+
+electron.app.on( "ready", start );
+electron.app.on( "window-all-closed", stop );
